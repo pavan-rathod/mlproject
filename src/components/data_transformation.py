95e4b58cda2414db06071a7a59f8d3adf30470e0
pavan-rathod <pavan2002rathod@gmail.com>
2025-07-16 22:38:51 +0530

    Model Trainer

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -45,9 +45,9 @@ class DataTransformation:
              
             cat_pipeline = Pipeline(
                 steps=[
-                    ('imputer',SimpleImputer(strategy="most_frequest")),
+                    ('imputer',SimpleImputer(strategy="most_frequent")),
                     ('one_hot_encoder',OneHotEncoder()),
-                    ('scaler',StandardScaler())
+                    ('scaler',StandardScaler(with_mean=False))
                 ]
             )
 
@@ -87,7 +87,7 @@ class DataTransformation:
 
             logging.info(f"Applying preprocessing object on training dataframe and testing dataframe.")
 
-            input_feature_train_arr = preprocessing_obj.fit_transform(input_feature_test_df)
+            input_feature_train_arr = preprocessing_obj.fit_transform(input_feature_train_df)
             input_feature_test_arr = preprocessing_obj.transform(input_feature_test_df)
 
             train_arr = np.c_[input_feature_train_arr, np.array(target_feature_train_df)]
